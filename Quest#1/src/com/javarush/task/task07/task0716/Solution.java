7ea4ab2285fec3ef32c7cbd32b182ab77a6a47fd
Артем <artemshabliy@gmail.com>
2017-01-17 23:11:16 +0200

    Quest 1

---
+++ Quest#1/src/com/javarush/task/task07/task0716/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task07.task0716;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Р или Л
+1. Создай список слов, заполни его самостоятельно.
+2. Метод fix должен:
+2.1. удалять из списка строк все слова, содержащие букву «р»
+2.2. удваивать все слова содержащие букву «л».
+2.3. если слово содержит и букву «р» и букву «л», то оставить это слово без изменений.
+2.4. с другими словами ничего не делать.
+
+Пример:
+роза
+лира
+лоза
+
+Выходные данные:
+лира
+лоза
+лоза
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("роза"); //0
+        list.add("лоза"); //1
+        list.add("лира"); //2
+        list = fix(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> list) {
+        //напишите тут ваш код
+        for (int i = 0; i < list.size();)
+        {
+            String str = list.get(i);
+            if (str.contains("л") && !str.contains("р"))
+            {
+                list.remove(i);
+                list.add(i, str);
+                list.add(i, str);
+                i+=2;
+            }
+            else if (!str.contains("л") && str.contains("р"))
+            {
+                list.remove(i);
+            }
+            else i++;
+        }
+        return list;
+    }
+}
---
+++ Quest#1/src/com/javarush/task/task07/task0724/Solution.java
@@ -0,0 +1,104 @@
+package com.javarush.task.task07.task0724;
+
+/* 
+Семейная перепись
+Создай класс Human с полями имя(String), пол(boolean), возраст(int), отец(Human), мать(Human).
+Создай объекты и заполни их так, чтобы получилось: Два дедушки, две бабушки, отец, мать, трое детей. Вывести объекты на экран.
+
+Примечание:
+Если написать свой метод String toString() в классе Human, то именно он будет использоваться при выводе объекта на экран.
+
+Пример вывода:
+Имя: Аня, пол: женский, возраст: 21, отец: Павел, мать: Катя
+Имя: Катя, пол: женский, возраст: 55
+Имя: Игорь, пол: мужской, возраст: 2, отец: Михаил, мать: Аня
+…
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Human grand1 = new Human("Gran1", true, 55);
+        Human grand2 = new Human("Grand2", true, 55);
+
+        Human baba1 = new Human("baba1", false, 55);
+        Human baba2 = new Human("baba2", false, 55);
+
+        Human father = new Human("father", true, 35,grand1, baba1);
+        Human mother = new Human("mother", false, 35,grand2, baba2);
+
+        Human baby1 = new Human("baby1", true, 5,father, mother);
+        Human baby2 = new Human("baby2", true, 5,father, mother);
+        Human baby3 = new Human("baby3", true, 5,father, mother);
+
+        System.out.println(grand1);
+        System.out.println(grand2);
+        System.out.println(baba1);
+        System.out.println(baba2);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(baby1);
+        System.out.println(baby2);
+        System.out.println(baby3);
+    }
+
+    public static class Human {
+        //напишите тут ваш код
+        String name;
+        boolean sex;
+        int age;
+        Human father;
+        Human mother;
+
+        public Human(String name, boolean sex, int age, Human father, Human mother) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.father = father;
+            this.mother = mother;
+        }
+
+        public Human(String name, boolean sex, int age) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
+
+        public String toString() {
+            String text = "";
+            text += "Имя: " + this.name;
+            text += ", пол: " + (this.sex ? "мужской" : "женский");
+            text += ", возраст: " + this.age;
+
+            if (this.father != null)
+                text += ", отец: " + this.father.name;
+
+            if (this.mother != null)
+                text += ", мать: " + this.mother.name;
+
+            return text;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
