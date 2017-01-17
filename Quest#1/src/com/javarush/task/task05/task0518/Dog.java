91ac5a8c713ce68b167ebe5cbb1b20150fb82196
Артем <Hippi_smartik1988>
2017-01-17 11:34:06 +0200

    Quest 1

---
+++ Quest#1/src/com/javarush/task/task05/task0518/Dog.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task05.task0518;
+
+/* 
+Создать класс Dog (собака) с тремя конструкторами:
+— Имя
+— Имя, рост
+— Имя, рост, цвет
+*/
+
+
+public class Dog {
+    //напишите тут ваш код
+    String name;
+    int height;
+    String color;
+
+    public Dog(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public Dog(String name, int height) {
+
+        this.name = name;
+        this.height = height;
+    }
+
+    public Dog(String name) {
+
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
