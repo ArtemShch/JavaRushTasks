0fc7d70b62ccf907009e9d363720ed4340759a60
Артем <Hippi_smartik1988>
2017-01-19 16:39:10 +0200

    Quest 2

---
+++ Quest#2/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Пять методов print с разными параметрами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static void print(Integer a)
+    {
+        System.out.println(a);
+    }
+    public static void print(Long a)
+    {
+        System.out.println(a);
+    }
+    public static void print(Double a)
+    {
+        System.out.println(a);
+    }
+    public static void print(Float a)
+    {
+        System.out.println(a);
+    }
+    public static void print(String a)
+    {
+        System.out.println(a);
+    }
+    //Напишите тут ваши методы
+}
